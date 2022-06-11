a56d4ff9a7b2c02f8b2edb22e45edd066e920219
mdikmetas <muratdikmetas@gmail.com>
2022-06-11 15:49:03 +0300

    Create deneme.py

---
+++ deneme.py
@@ -0,0 +1,31 @@
+module  = {
+    "Tokens": {
+        "USDC": '0x6a2d262d56735dba19dd70682b39f6be9a931d98',
+        "LAY": '0xc4335b1b76fa6d52877b3046eca68f6e708a27dd',
+
+        "dAPPL": "0xC10b2Ce6A2BCfdFDC8100Ba1602C1689997299D3",
+        "dTSLA": "0x17ace02e5c8814bf2ee9eaaff7902d52c15fb0f4",
+        "dAMZN": "0x1085b90544ff5c421d528aaf79cc65afc920ac79",
+        "dGOOGL": "0x9c169647471c1c6a72773cffc50f6ba285684803",
+    },
+
+
+    "AMM": {
+        "Pancake": {
+            "Factory": "0xA9473608514457b4bF083f9045fA63ae5810A03E",
+            "Router": "0xd7bbFc48c10dc43619E2C187451dDB55e1ECD66b",
+        },
+        "Twindex": {
+            "stables": {
+                "BUSD": "0xe9e7cea3dedca5984780bafc599bd69add087d56",
+                "DOLLY": "0xff54da7caf3bc3d34664891fc8f3c9b6dea6c7a5"
+            },
+            "Factory": "0x4e66fda7820c53c1a2f601f84918c375205eac3e",
+            "Router": "0x6B011d0d53b0Da6ace2a3F436Fd197A4E35f47EF",
+        },
+        "Ape": {
+            "Factory": "0x0841bd0b734e4f5853f0dd8d7ea041c241fb0da6",
+            "Router": "0xC0788A3aD43d79aa53B09c2EaCc313A787d1d607",
+        }
+    }
+}
